7f871d34aa06dd5c4dd9b14cc1dadf12b7a700a0
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-11-08 20:29:09 +0200

    add: docstring

--- map_detection/map_detection.py
+++ map_detection/map_detection.py
@@ -6,6 +6,30 @@ __all__ = ['generate_call_graphs']
 
 
 def generate_call_graphs(pptam_dir, tracing_dir, time_delta):
+    """Create call graphs and pipelines based on logs location.
+
+    Parameters
+    __________
+    pptam_dir - str,
+        Directory storing directories with pptam/locust configurations and
+        logs for each user (will be passed to detect_users()).
+    tracing_dir - str,
+        Directory containing tracing logs for each microservice (will be
+        passed to parse_logs()).
+    time_delta - datetime.timedelta,
+        Corrective timedelta to add to all timestamps in locust log (will be
+        passed to detect_users()).
+
+    Returns
+    _______
+    user_graphs : dict[str] -> networkx.MultiDiGraph,
+        For each user[_instance], a multigraph where nodes are services and
+        edges are calls between services keyed by the endpoint.
+    pipelines : dict[str] -> list[tuple[datetime, str, str, str]],
+        For each user[_instance], list of tuples containing the datetime of
+        the call, calling service, called service and called endpoint,
+        sorted by the time of call.
+    """
     user_boundaries, instance_boundaries = detect_users(pptam_dir, time_delta)
 
     # Get calls and pipelines for each user using logs of each service
