7e25dd9e53530489286217d2c1bbf2e9002bf60f
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2023-03-24 18:41:53 +0200

    add: handle ihr and rb detectors

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def data():
                 ah = 1.0
             else:
                 an = 1.0
-            nodes.append({"id":node, "title": node, "arc__normal": an,
+            nodes.append({"id":node, "title": node, "arc__f_normal": an,
                           "arc__frontend_candidate": ac,
                           "arc__frontend_violator": av,
                           "arc__frontend_healthy": ah})
@@ -56,6 +56,71 @@ def data():
         for edge in G.edges:
             edges.append({"id": id_, "source": edge[0], "target": edge[1]})
             id_ += 1
+    elif detector == "ihr":
+        databases = request.args.get('databases', None)
+        databases = set(databases.split(',')) if databases is not None else \
+                    set()
+        p = os.path.join("edgelists", edgelist)
+        G = map_detection.read_edgelist(p)
+        ihr_c, ihr_v, db_v, db_no_ihr = \
+            map_detection.detectors.information_holder_resource(G, databases)
+        ihr_c = {t[0] for t in ihr_c}
+        ihr_v = {t[0] for t in ihr_v}
+        for node in G.nodes:
+            a_n = a_ihr_c = a_ihr_v = a_db_h = a_db_v = a_db_no_ihr = 0.0
+            if node in databases:
+                if node in db_v:
+                    if node in db_no_ihr:
+                        a_db_v = 0.5
+                        a_db_no_ihr = 0.5
+                    else:
+                        a_db_v = 1.0
+                elif node in db_no_ihr:
+                    a_db_no_ihr = 1.0
+                else:
+                    a_db_h = 1.0
+            elif node in ihr_c:
+                a_ihr_c = 1.0
+            elif node in ihr_v:
+                a_ihr_v = 1.0
+            else:
+                a_n = 1.0
+            nodes.append({"id": node, "title": node, "arc__db_normal": a_n,
+                          "arc__ihr_candidate": a_ihr_c,
+                          "arc__ihr_violator": a_ihr_v,
+                          "arc__db_violator": a_db_v,
+                          "arc__db_no_ihr": a_db_no_ihr,
+                          "arc__db_healthy": a_db_h})
+        id_ = 0
+        for edge in G.edges:
+            edges.append({"id": id_, "source": edge[0], "target": edge[1]})
+            id_ += 1
+    elif detector == "request_bundle":
+        endpoint_threshold = request.args.get("endpoint_threshold", 2)
+        service_threshold = request.args.get("service_threshold", 2)
+        p = os.path.join("edgelists", edgelist)
+        bs, be = map_detection.detectors.request_bundle(p, service_threshold,
+                                                        endpoint_threshold)
+        G = map_detection.read_edgelist(p)
+        discovered = set()
+        for f, t, e, c in be:
+            if f not in discovered:
+                nodes.append({"id": f, "title": f, "arc__rb_v": 1.0,
+                              "arc__rc_n": 0.0})
+                discovered.add(f)
+            if t not in discovered:
+                nodes.append({"id": t, "title": t, "arc__rb_v": 1.0,
+                              "arc__rc_n": 0.0})
+                discovered.add(t)
+        for node in G.nodes:
+            if node not in discovered:
+                nodes.append({"id": node, "title": node, "arc__rb_v": 0.0,
+                              "arc__rc_n": 1.0})
+        id_ = 0
+        for edge in G.edges:
+            edges.append({"id": id_, "source": edge[0], "target": edge[1]})
+            id_ += 1
+
     else:
         return f"Unknown detector '{detector}'"
     return jsonify({"nodes": nodes, "edges": edges})
